d7795f141b3aaab6ffe2dde6944f266b6cffc85f
AHED200 <78029038+AHED200@users.noreply.github.com>
2022-12-11 01:49:08 +0300

    Create new avtivites files

---
+++ app/src/main/java/com/example/noteapp/Views/CreateNote.kt
@@ -0,0 +1,12 @@
+package com.example.noteapp.Views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.noteapp.R
+
+class CreateNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
---
+++ app/src/main/java/com/example/noteapp/Views/NoteDetails.kt
@@ -0,0 +1,12 @@
+package com.example.noteapp.Views
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.noteapp.R
+
+class NoteDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_details)
+    }
+}
