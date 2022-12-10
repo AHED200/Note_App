3cef000ac31c465f38b7387da27eb7548c0877e3
AHED200 <78029038+AHED200@users.noreply.github.com>
2022-12-11 01:47:23 +0300

    Finish backend in app

--- app/src/main/java/com/example/noteapp/Models/Interfaces/NoteDao.kt
+++ app/src/main/java/com/example/noteapp/Models/Interfaces/NoteDao.kt
@@ -1,6 +1,6 @@
 package com.example.noteapp.Models.Interfaces
 
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -10,11 +10,11 @@ import com.example.noteapp.Models.Note
 @Dao
 interface NoteDao {
     @Insert
-    fun insertNote(note: Note)
+    suspend fun insertNote(note: Note)
 
     @Delete
-    fun deleteNote(note: Note)
+    suspend fun deleteNote(note: Note)
 
     @Query("SELECT * FROM notes")
-    fun getAllNotes():MutableLiveData<Note>
+    fun getAllNotes():LiveData<List<Note>>
 }
--- app/src/main/java/com/example/noteapp/Repositories/NoteRepository.kt
+++ app/src/main/java/com/example/noteapp/Repositories/NoteRepository.kt
@@ -2,43 +2,33 @@ package com.example.noteapp.Repositories
 
 import NoteDatabase
 import android.content.Context
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.LiveData
 import com.example.noteapp.Models.Note
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 
-class NoteRepository {
+class NoteRepository(private var noteDatabase: NoteDatabase) {
 
-    companion object{
-        var noteDatabase:NoteDatabase?=null
-        var allNotes:MutableLiveData<Note>?=null
+    var allNotes: LiveData<List<Note>>? = null
 
-        fun initializeDB(context: Context):NoteDatabase{
-            return NoteDatabase.createDatabase(context)
+    fun getAllNotes(context: Context): LiveData<List<Note>>? {
+        allNotes = noteDatabase.noteDao().getAllNotes()
+
+        return allNotes
+    }
+
+    fun insertNote(context: Context, note: Note) {
+        CoroutineScope(IO).launch {
+            noteDatabase.noteDao().insertNote(note)
+        }
+    }
+
+    fun deleteNote(context: Context, note: Note) {
+
+        CoroutineScope(IO).launch {
+            noteDatabase.noteDao().deleteNote(note)
         }
 
-        fun getAllNotes(context: Context):MutableLiveData<Note>?{
-            noteDatabase= initializeDB(context)
-            allNotes= noteDatabase!!.noteDao().getAllNotes()
-
-            return allNotes
-        }
-
-        fun insertNote(context: Context, note:Note){
-            noteDatabase = initializeDB(context)
-
-            CoroutineScope(IO).launch {
-                noteDatabase!!.noteDao().insertNote(note)
-            }
-        }
-
-        fun deleteNote(context: Context, note:Note){
-            noteDatabase = initializeDB(context)
-
-            CoroutineScope(IO).launch {
-                noteDatabase!!.noteDao().deleteNote(note)
-            }
-        }
     }
 }
