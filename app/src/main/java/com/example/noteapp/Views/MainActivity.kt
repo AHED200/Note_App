b1c9ccd65277c4b7d612a304fdc266d67a165eb7
AHED200 <78029038+AHED200@users.noreply.github.com>
2022-12-11 16:03:48 +0300

    Finish MainActivity

--- app/src/main/java/com/example/noteapp/Views/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/Views/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.noteapp.Views
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.noteapp.R
+import com.example.noteapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
     }
 }
